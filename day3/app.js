f698dd972c102445177423945771764cf862f4b7
Abiola <49586787+Abiola-Farounbi@users.noreply.github.com>
2020-05-17 02:31:41 +0100

    Update app.js

--- day3/app.js
+++ day3/app.js
@@ -1,31 +1,37 @@
-var form=document.getElementById('formId');
-var bmi=0;
-var weight=parseFloat(document.getElementById('weight1').value);
+var submitted=document.querySelector('.submit');
 
-var height=parseFloat(document.getElementById('height1').value);
-form.addEventListener("click",calculateBmi);
-       
+submitted.addEventListener("click",calculateBmi);    
+
+function calculateBmi(e){
+    var weight=document.getElementById('weight1').value;
+    var height=document.getElementById('height1').value;
+    var output=document.getElementById('result');
+    var bmi=(parseInt(weight))/((parseInt(height)/100)*(parseInt(height)/100));
+      bmi=bmi.toFixed(2);
+    
+      console.log(bmi)
+        if(bmi < 18.5){ 
+           output .innerHTML=` Your Body Mass Index is:  ${bmi}- (Underweight)`
+        }
+        else if(bmi > 18.5 && bmi < 25){
+            output.innerHTML=`Your Body Mass Index is:  ${bmi}- (Normal Weight)`
+        }
+        else if (bmi > 25){
+            output.innerHTML=`Your Body Mass Index is: ${bmi}- (Over Weight)`
+        }
+    
+   
         
-        function calculateBmi(e){
-                bmi=((weight)/((height/100)*(height/100)));
-                bmi=bmi.toFixed(2);
-                if(bmi < 18.5){  
-                    document.getElementById('result').innerHTML=` Your Body Mass Index is:  ${bmi}- (Underweight)`
-                }
-                else if(bmi > 18.5 || bmi < 25){
-                    document.getElementById('result').innerHTML=`Your Body Mass Index is:  ${bmi}- (Normal Weight)`
-                }
-                else if (bmi > 25){
-                    document.getElementById('result').innerHTML=`Your Body Mass Index is: ${bmi}- (Over Weight)`
-                }
-            
-           
-                
-     
-        
-    e.preventDefault();
+
+
+ e.preventDefault();
 
 }
+
+
+    
+
+
 
     
 
